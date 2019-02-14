4c7fa5abc82baf371b86c821f373096b0b79e373
Tony KINO <tony.kino.3@gmail.com>
2019-02-14 16:29:33 +0100

    Specs student model

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -15,7 +15,30 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  it { is_expected.to have_db_column(:first_name).of_type(:string).with_options(null: false) }
-  it { is_expected.to have_db_column(:last_name).of_type(:string).with_options(null: false) }
-  it { is_expected.to have_db_column(:birth_date).of_type(:date) }
+  subject do
+    build(:student)
+  end
+
+  describe 'it is creatable' do
+    let(:student) { create(:student) }
+
+    it { expect(student.id).not_to be_nil }
+    it { expect(student.first_name).not_to be_nil }
+  end
+
+  describe 'Database' do
+    it { is_expected.to have_db_column(:id).of_type(:integer) }
+    it { is_expected.to have_db_column(:first_name).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:last_name).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:birth_date).of_type(:date) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_length_of(:first_name).is_at_least(2).is_at_most(40) }
+    it { is_expected.to validate_length_of(:last_name).is_at_least(2).is_at_most(40) }
+  end
 end
