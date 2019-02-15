a00bee03098ab9ae76aa5755f5956b6c4ce25d15
charles_mahaco <charles.belpois@gmail.com>
2019-02-15 17:30:56 +0100

    tests student-controller

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -17,4 +17,12 @@ RSpec.describe StudentsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "DELETE #destroy" do
+    it "redirects to students_path" do
+      student = create(:student)
+      get :destroy, params: { id: student.id }
+      expect(response).to redirect_to(students_path)
+    end
+  end
 end
