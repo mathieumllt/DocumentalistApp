910f72dec416167a021c29bf536c84f5b06b4d7c
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-02-18 16:30:53 +0100

    Add modification for rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
   resources :students do
     collection { post :import }
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :students
-
 end
