870aafaeecff950e614984355994910809c7d68e
stephan-dev <stephan-dev@outlook.fr>
2019-02-21 10:57:50 +0100

    annotate new model?

--- app/models/students_list.rb
+++ app/models/students_list.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: students_lists
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
 class StudentsList < ApplicationRecord
 end
--- spec/factories/students_lists.rb
+++ spec/factories/students_lists.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: students_lists
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
 FactoryBot.define do
   factory :students_list do
   end
