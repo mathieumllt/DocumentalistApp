bb35d684def15b634fb7b06fc7bcdb1f8e637daf
charles_mahaco <charles.belpois@gmail.com>
2019-02-21 19:38:28 +0100

    Sidekiq + active Storage OK

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: lists
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class List < ApplicationRecord
   has_one_attached :students_csv
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: lists
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
