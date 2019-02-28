805d1f65acaecca38c8d46bebdd4be0291c11f9c
stephan-dev <stephan-dev@outlook.fr>
2019-02-28 23:07:22 +0100

    attempt rescue save error in worker

--- app/jobs/import_csv_job.rb
+++ app/jobs/import_csv_job.rb
@@ -5,7 +5,24 @@ class ImportCsvJob < ApplicationJob
 
   def import(students_array)
     students_array.each do |line|
-      Student.create line
+      new_student = Student.new line
+      unless new_student.save?
+        puts '//////////////'
+        puts
+        puts 'below are the possible detailed error messages'
+        puts new_student.save.errors.messages
+        puts 'there was an error'
+        puts
+        puts '//////////////'
+      end
+    rescue StandardError
+      puts '//////////////'
+      puts
+      puts 'below is possible new_student.save.errors.messages'
+      puts new_student.save.errors.messages
+      puts 'there was an error'
+      puts
+      puts '//////////////'
     end
   end
 
