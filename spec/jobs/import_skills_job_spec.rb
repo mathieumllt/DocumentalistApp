b45ac57f4452913a21ab3eca974dccc9686dc4f2
charles_mahaco <charles.belpois@gmail.com>
2019-03-06 14:01:52 +0100

    commented code + chenged vriable names

--- spec/jobs/import_skills_job_spec.rb
+++ spec/jobs/import_skills_job_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe ImportSkillJob, type: :job do
+RSpec.describe ImportSkillsJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
