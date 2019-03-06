fc15b4006c377265df6b1449edce87e98049d861
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-03-06 12:19:04 +0100

    again rubo

--- app/models/skill_student.rb
+++ app/models/skill_student.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: skill_students
+#
+#  id         :bigint(8)        not null, primary key
+#  skill_id   :bigint(8)
+#  student_id :bigint(8)
+#  origin     :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class SkillStudent < ApplicationRecord
 end
