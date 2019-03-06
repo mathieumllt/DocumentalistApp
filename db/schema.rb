a0a0c428b732ee2de5c55dde340ba01e8110637c
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-03-06 12:27:54 +0100

    add change after rubo

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_03_05_145145) do
-=======
-ActiveRecord::Schema.define(version: 2019_02_28_150200) do
->>>>>>> dev
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
