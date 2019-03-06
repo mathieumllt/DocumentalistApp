b111fd4648a365463d7bcad703d5e531ed4cd880
stephan-dev <stephan-dev@outlook.fr>
2019-03-06 15:41:23 +0100

    end fix merge

--- app/jobs/import_students_job.rb
+++ app/jobs/import_students_job.rb
@@ -7,10 +7,13 @@ class ImportStudentsJob < ApplicationJob
     students_array.each do |row_and_index|
       # row_and_index is the csv row and its associated line
       student = Student.new(row_and_index[0])
-      student.save
-      if !student.save
-        # if the worker fail to create a student, we store the error with its associated error description
-        ImportError.create(line: row_and_index[1], error_name: "Students", data_type: row_and_index[0].to_a, error_type: "Doublon")
+
+      begin
+        student.save!
+      # if the worker fails to create a student, we store the error with its associated error description
+      rescue StandardError => e
+        puts e
+        ImportError.create(line: row_and_index[1], error_name: "Students", data_type: row_and_index[0].to_a, error_type: e)
       end
     end
   end
